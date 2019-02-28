e2f7994fe7178c57941519eec98b37915680ddb1
SeMbA Max <semba.max@gmail.com>
2019-02-28 12:43:17 +0200

    update ViewModelFactory

--- app/src/main/java/com/semba/androidsamples/ArchPagination/MainScreen/ViewModelFactory.kt
+++ app/src/main/java/com/semba/androidsamples/ArchPagination/MainScreen/ViewModelFactory.kt
@@ -1,5 +1,7 @@
 package com.semba.androidsamples.ArchPagination.MainScreen
 
+import android.app.Application
+import androidx.annotation.VisibleForTesting
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 import com.semba.androidsamples.ArchPagination.Data.Repository
@@ -9,16 +11,15 @@ import javax.inject.Inject
 
 class ViewModelFactory @Inject constructor(private val repository: Repository): ViewModelProvider.Factory {
 
-    override fun <T : ViewModel?> create(modelClass: Class<T>): T {
-        return if (modelClass.isAssignableFrom(NewsScreenViewModel::class.java))
-        {
-            NewsScreenViewModel(this.repository) as T
-        }
-        else
-        {
-            val msg = "ViewModel not found"
-            Logger().e(msg)
-            throw IllegalArgumentException(msg)
-        }
+    override fun <T : ViewModel?> create(modelClass: Class<T>) = with(modelClass) {
+        when {
+            isAssignableFrom(NewsScreenViewModel::class.java) ->
+                NewsScreenViewModel(repository)
+            else-> {
+                val msg = "Unknown ViewModel Class : ${modelClass.name}"
+                Logger().e(msg)
+                throw IllegalArgumentException(msg)
+            }
+        } as T
     }
 }
